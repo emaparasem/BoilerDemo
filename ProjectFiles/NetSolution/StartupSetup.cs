7f4baae9351b15437c9dd3d0649809f0da16ca69
e.parolotti@asem.it <emaparasem>
2022-05-24 12:13:34 +0200

    Added StartupSetup method

---
+++ ProjectFiles/NetSolution/StartupSetup.cs
@@ -0,0 +1,44 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using QPlatform.NetLogic;
+using QPlatform.HMIProject;
+using QPlatform.WebUI;
+using QPlatform.UI;
+using QPlatform.NativeUI;
+using QPlatform.Recipe;
+using QPlatform.Datalogger;
+using QPlatform.EventLogger;
+using QPlatform.SQLiteStore;
+using QPlatform.Store;
+using QPlatform.OPCUAClient;
+using QPlatform.CoreBase;
+using QPlatform.CommunicationDriver;
+using QPlatform.Alarm;
+using QPlatform.Core;
+#endregion
+
+public class StartupSetup : BaseNetLogic
+{
+    public override void Start()
+    {
+        var highLevel = Project.Current.GetVariable("OPC-UA/OPCUAClient/Objects/Demo/BoilerDemo/Boiler1/FillLevelSensor/FillLevel/EURange/High");
+        if (highLevel != null)
+            highLevel.Value = 100;
+        var minLevel = Project.Current.GetVariable("OPC-UA/OPCUAClient/Objects/Demo/BoilerDemo/Boiler1/FillLevelSensor/FillLevel/EURange/Low");
+        if (minLevel != null)
+            minLevel.Value = 0;
+        var highTemperature = Project.Current.GetVariable("OPC-UA/OPCUAClient/Objects/Demo/BoilerDemo/Boiler1/TemperatureSensor/Temperature/EURange/High");
+        if (highTemperature != null)
+            highTemperature.Value = 90;
+        var lowTemperature = Project.Current.GetVariable("OPC-UA/OPCUAClient/Objects/Demo/BoilerDemo/Boiler1/TemperatureSensor/Temperature/EURange/Low");
+        if (lowTemperature != null)
+            lowTemperature.Value = -10;
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
